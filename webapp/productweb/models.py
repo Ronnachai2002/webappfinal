e8c1433a67ff39d7b7fd7dd64ae18329ba17a816
ronnachai2002 <ronnachai.bu.64@ubu.ac.th>
2023-12-18 14:16:27 +0700

    update

--- webapp/productweb/models.py
+++ webapp/productweb/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Item(models.Model):
     title = models.CharField(max_length=50)
-    size = models.CharField(max_length=50)
-    description = models.CharField(max_length=255, default='No description')
-    matter = models.CharField(max_length=50, default='No description')
+    size = models.CharField(max_length=500)
+    description = models.TextField(default='No description')
+    material = models.CharField(max_length=50, default='No description')
+    price = models.CharField(max_length=50)
 
 class ItemImage(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='images')
